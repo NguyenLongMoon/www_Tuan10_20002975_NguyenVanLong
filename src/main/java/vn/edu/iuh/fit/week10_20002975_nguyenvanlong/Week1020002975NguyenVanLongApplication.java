04a523f462de85b64199278c4f85971852766721
NguyenLongMoon <longsky0912624119@gmail.com>
2023-12-04 15:53:58 +0700

    cap nhat config

---
+++ src/main/java/vn/edu/iuh/fit/week10_20002975_nguyenvanlong/Week1020002975NguyenVanLongApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.week10_20002975_nguyenvanlong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1020002975NguyenVanLongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week1020002975NguyenVanLongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/week10_20002975_nguyenvanlong/Week1020002975NguyenVanLongApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.week10_20002975_nguyenvanlong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1020002975NguyenVanLongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
